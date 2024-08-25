6c5d4d94390345d0db9c87975379ce0397231269
samarthyasurve17 <samarthyasurve17@gmail.com>
2024-08-25 09:08:13 +0000

    zala

---
+++ script.js
@@ -0,0 +1,46 @@
+// alert("hello");
+// console.log( prompt("where are u ?"))
+//console actually holds the given data in the console section
+// console.log("namsate"+" samarthya")
+// con-cate-nattion
+// console.log("=kasa kai" ,prompt("ENTER YOUR NAME"))
+// console.log(5 + prompt("ENTER YOUR number"))
+// let jar=prompt("enter your juice")
+// console.log((jar), "juice") 
+
+// let sam = "sam sullek"
+//  sam = "sam surve"  /me hyat variable sam che value change kele ahhe
+// console.log("sam");
+// let fruit
+
+// console.log( 2+ "A")
+// let UserKaInput = Number(prompt("enter a number of your choice"));     
+//promt madhe lehela he nehmi string madhe asta so tyala number madhe convert kela "Number" he use krun sonappun addition perform kru shakto 
+// console.log(5 + (UserKaInput))  
+
+// variable//   ("let" )  that is not fixedd is a variable//
+//variable// ("constant") that is fixed and cannot be change//
+
+// const fruit = null;
+// console.log(10*fruit);
+
+//implicit typeconversion
+//type coercion
+
+//undefined=automatic ( let fruit ;=> undefined) implicit
+//null = manual ( const fruit ; => null) expliciit
+// let a = 3
+// function sum() {
+//     let b = 2
+//     console.log(a+b);
+// }
+// // console.log(b)
+
+// function sum(num1,num2){
+//  return num1+num2
+// }
+// let a = sum(1,2)
+// console.log (a)
+
+function double(num){
+    return num *2}
